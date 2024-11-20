75c45458f0ac3616c9adadea77606e8a070d25aa
Mpuuga Tyrone <104665271+MpuugaTyroneVictorWoods2006@users.noreply.github.com>
2024-11-20 10:07:09 +0300

    Worked on Tasks views and Task Item listiing

--- Task_App/tasks/views.py
+++ Task_App/tasks/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+tasks = ["Go shopping", "Buy Car", "Eat Lunch"]
 def index(request):
-    return render(request, 'tasks/index.html')
+    return render(request, 'tasks/index.html', {
+        "tasks": tasks
+    })
