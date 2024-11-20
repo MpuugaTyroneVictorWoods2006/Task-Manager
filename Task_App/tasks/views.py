4c153c79c11526fc5d2d4a1fc1af4f3916d6a787
Mpuuga Tyrone <104665271+MpuugaTyroneVictorWoods2006@users.noreply.github.com>
2024-11-20 12:45:50 +0300

    Designed Task item and added Buttons without icons

--- Task_App/tasks/views.py
+++ Task_App/tasks/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-tasks = ["Go shopping", "Buy Car", "Eat Lunch"]
+tasks = ["Go shopping", "Buy Car", "Eat Lunch", "Help noob"]
 def index(request):
     return render(request, 'tasks/index.html', {
         "tasks": tasks
