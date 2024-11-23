d226551886a98b1ba895cae5694420a2f3cb0eb5
Mpuuga Tyrone <104665271+MpuugaTyroneVictorWoods2006@users.noreply.github.com>
2024-11-23 16:45:17 +0300

    Worked on the submit functionality

--- Task_App/tasks/views.py
+++ Task_App/tasks/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-tasks = ["Go shopping", "Buy Car", "Eat Lunch", "Help noob"]
+tasks = []
 def index(request):
-    return render(request, 'tasks/index.html', {
-        "tasks": tasks
-    })
+    if request.method == 'POST':
+        # Get the text from the form input
+        task_item = request.POST.get('task')
+        if task_item:  # Only add if it's not empty
+            tasks.append(task_item)
+    return render(request, 'tasks/index.html', {'tasks': tasks})
