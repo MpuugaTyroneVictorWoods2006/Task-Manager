c33e0fd9885a2341e34747e9222a118d040ec3e7
Mpuuga Tyrone <104665271+MpuugaTyroneVictorWoods2006@users.noreply.github.com>
2024-12-05 13:17:16 +0300

    Used dictionary instead of list datatype

--- Task_App/tasks/views.py
+++ Task_App/tasks/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import redirect
 
-tasks = []
+tasks = {}
+task_counter = 0
 def index(request):
+    global task_counter
+    global tasks_list
     if request.method == 'POST':
-        # Get the text from the form input
         task_item = request.POST.get('task')
         if task_item:  # Only add if it's not empty
-            tasks.append(task_item)
+            task_counter += 1
+            tasks[task_item] = task_counter
+            tasks_list = list(tasks.values())
     return render(request, 'tasks/index.html', {'tasks': tasks})
